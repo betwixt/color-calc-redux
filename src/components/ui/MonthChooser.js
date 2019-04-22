3a3359746000cccfa894a73c9976950c073171de
betwixt <bcbuenafe@gmail.com>
2019-04-22 06:26:34 +0100

    Updates for newer react version, prop-types & css comments

--- src/components/ui/MonthChooser.js
+++ src/components/ui/MonthChooser.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import { months } from '../colorData';
 import Square from './Square'
 
