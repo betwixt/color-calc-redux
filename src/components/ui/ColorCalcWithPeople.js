cc7c0cb44d30ef456ed4546b4e859090b7b690c8
betwixt <bcbuenafe@gmail.com>
2019-11-02 22:17:40 +0100

    Add nav bar with links to other projects

--- src/components/ui/ColorCalcWithPeople.js
+++ src/components/ui/ColorCalcWithPeople.js
@@ -9,20 +9,30 @@ import CalcInput from '../containers/CalcInput';
 
 import './ColorCalcWithPeople.css';
 
+// NOTE: When dev environment uses create-react-app server, urls in nav bar will need to 
+// point to separate django server at "http://127.0.0.1:8000"
+
 class ColorCalcWithPeople extends Component {
 
   render() {
     return (
 
      <div>		
-		<Container>
-		  <Intro />
-		  <Grid padded="horizontally">
-			<SubjectSelection />
-			<Subject />
-			<CalcInput />	
-		    <ColorPane />			
-		  </Grid>
+		<div class="ui mini borderless menu" style={{marginLeft: '15px'}}>
+			<a class="item" href="/">Home</a>
+			<a class="active item" href="#">Colors w/ Data</a>				
+			<a class="item" href="/weather/">Multi Weather</a>
+			<a class="item" href="/colors1/">Color Calc 1</a>
+		</div>
+			<Container>
+
+			<Intro />
+			<Grid>
+				<SubjectSelection />
+				<Subject />
+				<CalcInput />	
+				<ColorPane />			
+			</Grid>
 
 	    </Container>
     </div>
--- src/components/ui/Parts.js
+++ src/components/ui/Parts.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { Icon, Label, Accordion, Grid, } from 'semantic-ui-react';
+import { Image, Grid, } from 'semantic-ui-react';
+import hearts from './rainbow-hearts-touching.jpg';
 
-
-// Section with header explaining background, list of saved persons/bdays, button to choose 
-// a bday not yet saved
-
-//			<div className="App-header">
 
 class Intro extends Component {
   render() {
+
     return ( 
-			<header>
-				Banner with background info and nice image 
-			</header>
+
+		  <Grid id="header">
+			<Grid.Column width={3}>
+				<Image src={hearts} size='small' id="banner-img" />
+			</Grid.Column>
+			<Grid.Column width={12}>
+				  <h1 style={{paddingTop: '25px', font: 'Satisfy', color: 'DodgerBlue'}}>Your Daily Color</h1>
+				  <p>
+					Background:  According to numerology, our birthday is the key to calculating a personal
+					number vibration for any given day, as well as the personal color associated with
+					that number. Colors can remind us of principles and attitudes that bring us more in tune
+					with Life.
+				  </p>
+				  <p>
+				    The calculations used here are based on Louise Hay's book, <i>Colors and Numbers</i>.  
+					Start by entering a person's birthday, then choose the day or days for calculating a 
+					personal color. To learn the themes associated with a color, click on <small><u>More Info</u></small> or a day within the Week table.
+				  </p>
+			</Grid.Column>
+		  </Grid>
+
+		
+		
     );
 
   }
